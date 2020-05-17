99643d12e4bda8fa5ff416b2caa08d5c36647f5a
Tianyi Chen <TianyiChen@users.noreply.github.com>
2020-05-17 00:05:30 -0700

    autofix for linking errors

--- dfsan-inject/genInjectTask.py
+++ dfsan-inject/genInjectTask.py
@@ -4,13 +4,14 @@ sys.stdout=open('/tmp/task.txt','w')
 print('task.out.c patch.txt')
 troot='../../bingo-ci-experiment/benchmark/'+sys.argv[1]+'/sparrow-out/'
 print(troot,file=sys.stderr)
-alarms=open(troot+'taint/datalog/DUEdge.facts')#60k
-#alarms=open(troot+'taint/datalog/Alarm.facts')
-nodes=json.load(open(troot+'node.json','r'))['nodes']
+#alarms=open(troot+'taint/datalog/DUEdge.facts')#60k
+alarms=open(troot+'taint/datalog/Alarm.facts')
+s=open(troot+'node.json','rb').read()
+nodes=json.loads(s.decode('utf-8','ignore'))['nodes']
 i=0
 import random
 for _ in alarms:
-	if random.random()>.001:continue
+	#if random.random()>.001:continue
 	i+=1
 	print(_,end='',file=sys.stderr)
 	for n in _.split():
--- dfsan-plugin/go.sh
+++ dfsan-plugin/go.sh
@@ -9,16 +9,18 @@ cd $WORKDIR
 #time clang-11  -fsanitize=dataflow  -Xclang -load -Xclang $(pwd)/dfsan-plugin.so -Xclang -add-plugin -Xclang ToyClangPlugin test.cpp -o $WORKDIR/test
 {
 python3 -m tclib download \
-https://ftp.gnu.org/gnu/wget/wget-1.19.5.tar.gz  wget-1.19.5.tar.gz b39212abe1a73f2b28f4c6cb223c738559caac91d6e416a6d91d4b9d55c9faee
+https://ftp.gnu.org/gnu/tar/tar-1.28.tar.gz tar-1.28.tar.gz any
+#https://ftp.gnu.org/gnu/tar/tar-1.30.tar.gz tar-1.30.tar.gz any
+#https://sourceforge.net/projects/optipng/files/OptiPNG/optipng-0.5.3/optipng-0.5.3.tar.gz/download optipng-0.5.3.tar.gz fa910c7dc8dbe29323494097255f034d374f8ef0e42ace3e3855408e014319e5
+#https://ftp.gnu.org/gnu/wget/wget-1.19.5.tar.gz  wget-1.19.5.tar.gz b39212abe1a73f2b28f4c6cb223c738559caac91d6e416a6d91d4b9d55c9faee
 #https://curl.haxx.se/download/curl-7.69.1.tar.gz curl-7.69.1.tar.gz 01ae0c123dee45b01bbaef94c0bc00ed2aec89cb2ee0fd598e0d302a6b5e0a98
 #https://curl.haxx.se/download/curl-7.41.0.tar.gz curl-7.41.0.tar.gz
 #https://ftp.gnu.org/gnu/wget/wget-1.11.4.tar.gz wget-1.11.4.tar.gz 7315963b6eefb7530b4a4f63a5d5ccdab30078784cf41ccb5297873f9adea2f3
 #https://ftp.gnu.org/gnu/sed/sed-4.2.2.tar.gz sed-4.2.2.tar.gz fea0a94d4b605894f3e2d5572e3f96e4413bcad3a085aae7367c2cf07908b2ff
-#https://sourceforge.net/projects/optipng/files/OptiPNG/optipng-0.5.3/optipng-0.5.3.tar.gz/download optipng-0.5.3.tar.gz fa910c7dc8dbe29323494097255f034d374f8ef0e42ace3e3855408e014319e5
 echo >$WORKDIR/plog.log
 echo >$WORKDIR/loc_vars.txt
 
-APP=wget-1.19.5
+APP=tar-1.28
 rm -rf $WORKDIR/$APP
 export DFSAN_OPTIONS=warn_unimplemented=0
 export CC="clang-11 -I$INCLUDE -fsanitize=dataflow -fsanitize-blacklist=/tmp/openssl-list.txt\
@@ -36,7 +38,15 @@ popd
 python3 $AHOME/../dfsan-inject/applyPatch.py $WORKDIR/$APP
 pushd $WORKDIR/$APP
 unset DFPG_MODE
-make
+if ! make 2>makeerr.txt;then
+	crc=$(crc32 makeerr.txt)
+	retry=1
+	while ((retry<=10)) && [ ! make 2>makeerr.txt ]
+	do
+		python3 $AHOME/autofix.py
+		((++retry))
+	done
+fi
 popd
 
 }
