b96404cf2cdab0d5f7ccf7d1fe83e6e47ee604d4
Tianyi Chen <TianyiChen@users.noreply.github.com>
2020-06-12 14:09:13 -0700

    use custom llvm

--- init.sh
+++ init.sh
@@ -1,1 +1,4 @@
+export LLVM_VIRT=/tmp/llvm-project/virtualroot
+export PATH=$LLVM_VIRT/bin:$PATH
+export LD_LIBRARY_PATH=$LLVM_VIRT/lib:$LD_LIBRARY_PATH
 cp dfsan-inject/openssl-list.txt /tmp/openssl-list.txt
