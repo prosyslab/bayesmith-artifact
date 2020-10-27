a16c912a8a08f881e696c7c4e13d7e61abba3bb7
Tianyi Chen <>
2020-10-26 22:40:32 -0700

    fix

--- utils/bingosum.py
+++ utils/bingosum.py
@@ -12,14 +12,17 @@ for x in sorted(os.listdir(os.environ['BINGO'])):
 	if 'stats' in x and x.startswith(prefix):
 		a,b=parsesname(x)
 		if a not in data:data[a]=defaultdict(lambda:'null,null')
-		y=open(os.environ['BINGO']+'/'+x).readlines()
-		inv=y[1].split()[-3]
-		#print(inv)
-		iters=len(y)-1
-		if int(y[-1].split()[3])==0: #bug not found
-			iters='>'+str(iters)
-		#print(iters)
-		data[a][b]=f'{inv},{iters}'
+		try:
+			y=open(os.environ['BINGO']+'/'+x).readlines()
+			inv=y[1].split()[-3]
+			#print(inv)
+			iters=len(y)-1
+			if int(y[-1].split()[3])==0: #bug not found
+				iters='>'+str(iters)
+			#print(iters)
+			data[a][b]=f'{inv},{iters}'
+		except:
+			pass
 print('Summary,',','.join([x+'inv,'+x+'iters' for x in RTYPES]))
 for x in data:
 	print(x,end='')
