7d78aae14110e2bc04852b2d7340786d0167c47a
Tianyi Chen <TianyiChen@users.noreply.github.com>
2020-11-10 16:31:24 -0800

    change order

--- utils/env.sh
+++ utils/env.sh
@@ -1,3 +1,3 @@
-BENCHMARKi=(tar-1.28 sort-7.2 readelf-2.24 grep-2.19 sed-4.3 bc-1.06 libtasn1-4.3 cflow-1.5 patch-2.7.1 gzip-1.2.4a)
+BENCHMARKi=(bc-1.06 cflow-1.5 grep-2.19 gzip-1.2.4a libtasn1-4.3 patch-2.7.1 readelf-2.24 sed-4.3 sort-7.2 tar-1.28 )
 BENCHMARKt=(optipng-0.5.3 latex2rtf-2.1.1 shntool-3.0.5)
 BENCHMARKa=("${BENCHMARKi[@]}" "${BENCHMARKt[@]}")
