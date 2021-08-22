feb4c7a4ae708ec031d20ba1084fbc3bd6937474
hyunsukimsokcho <hskimsokcho@gmail.com>
2021-08-22 23:51:39 +0900

    [DynaBoost] fix

--- update-scripts/bingo/src/generator.dynaboost.ml
+++ update-scripts/bingo/src/generator.dynaboost.ml
@@ -23,13 +23,13 @@ let main argv =
       ( if Array.length argv > 5 then
         let pgm = argv.(5) in
         Datalog.of_file (dl_file_dir ^ "TBufferOverflow." ^ pgm ^ ".dl") ""
-      else Datalog.make Buffer_rules.buffer_overflow_rules )
+      else Datalog.of_file (dl_file_dir ^ "BufferOverflow.dl") "" )
       |> BNet.generate_named_cons sparrow_out_dir analysis_type bnet_dir
   | "taint" ->
       ( if Array.length argv > 5 then
         let pgm = argv.(5) in
         Datalog.of_file (dl_file_dir ^ "TIntegerOverflow." ^ pgm ^ ".dl") ""
-      else Datalog.make Integer_rules.integer_overflow_rules )
+      else Datalog.of_file (dl_file_dir ^ "IntegerOverflow.dl") "" )
       |> BNet.generate_named_cons sparrow_out_dir analysis_type bnet_dir
   | _ -> assert false
 
