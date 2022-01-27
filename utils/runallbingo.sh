c99052a16ea3aae003d9a58c7beb3c3bf21a262e
hyunsukimsokcho <hskimsokcho@gmail.com>
2022-01-28 03:31:14 +0900

    [Script] update

--- utils/runallbingo.sh
+++ utils/runallbingo.sh
@@ -7,7 +7,7 @@ fi
 ./runbingo.sh bc-1.06 /tmp/bc-1.06/ interval ${PREFIX}bc $BAYESMITH_OPT >/dev/null 2>/dev/null &
 ./runbingo.sh cflow-1.5 /tmp/cflow-1.5/ interval ${PREFIX}cflow $BAYESMITH_OPT >/dev/null 2>/dev/null &
 ./runbingo.sh grep-2.19 /tmp/grep-2.19/ interval ${PREFIX}grep $BAYESMITH_OPT > /dev/null 2> /dev/null &
-./runbingo.sh gzip-1.2.4a /tmp/gzip-1.2.4a/ interval ${PREFIX}gzip $BAYESMITH_OPT > /dev/null 2> /dev/null &
+ulimit -s unlimited && ./runbingo.sh gzip-1.2.4a /tmp/gzip-1.2.4a/ interval ${PREFIX}gzip $BAYESMITH_OPT > /dev/null 2> /dev/null &
 ./runbingo.sh patch-2.7.1 /tmp/patch-2.7.1/ interval ${PREFIX}patch $BAYESMITH_OPT > /dev/null 2> /dev/null &
 ./runbingo.sh readelf-2.24 /tmp/readelf-2.24/ interval ${PREFIX}readelf $BAYESMITH_OPT >/dev/null 2>/dev/null &
 ./runbingo.sh sed-4.3 /tmp/sed-4.3/ interval ${PREFIX}sed $BAYESMITH_OPT >/dev/null 2>/dev/null &
